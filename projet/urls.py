80206f57a82c61dc7bfa01d01a1d3f750497e763
Hactogeek <tony.marteau@outlook.com>
2017-02-02 18:35:56 +0100

    Correction de problème de redirection d'url

--- projet/urls.py
+++ projet/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^socialNetwork/', include('socialNetwork.urls')),
+	url(r'^socialnetwork/', include('socialnetwork.urls')),
     url(r'^admin/', admin.site.urls),
 ]

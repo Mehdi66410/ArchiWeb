494b30b617e4ca461c07902a04a70b053c18e34b
Hactogeek <tony.marteau@outlook.com>
2017-02-02 18:33:23 +0100

    Système de d'inscription et de connexion

--- projet/settings.py
+++ projet/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Include BOOTSTRAP3_FOLDER in path
+BOOTSTRAP3_FOLDER = os.path.abspath(os.path.join(BASE_DIR, '..', 'bootstrap3'))
+if BOOTSTRAP3_FOLDER not in sys.path:
+    sys.path.insert(0, BOOTSTRAP3_FOLDER)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -31,12 +36,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #'socialnetwork.apps.SocialnetworkConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'projet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/Users/Hactogeek/Documents/L3SPI/ArchiWeb/projet/projet/socialnetwork/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+# Importation des bibiliothèques
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+# Importation des modèles
+from django.contrib.auth.models import User
+
+# Importation des formulaires
+from .forms import loginForm, registerForm
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+	if not request.user.is_authenticated:
+		return redirect(connexion)
+	else:
+		return HttpResponse('Bienvenue')
+
+def connexion(request):
+	if request.method == 'POST':
+		user = authenticate(username=request.POST['username'],
+			password=request.POST['password'])
+		if user is not None:
+			login(request, user)
+			return redirect(index)
+		else:
+			return HttpResponse('Erreur de login ou de mot de passe')
+	else:
+		form = loginForm()
+	return render(request, 'socialnetwork/login.html', {'form': form})
+
+def inscription(request):
+	if request.method == 'POST':
+		form = registerForm(request.POST)
+		if form.is_valid():
+			user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'],
+				email=request.POST['email'], first_name=request.POST['first_name'], last_name=request.POST['last_name'])
+			user.save()
+			return redirect(connexion)
+	else:
+		form = registerForm()
+	return render(request, 'socialnetwork/registration.html', {'form': form})

8bb8096ee5f3a27257d408cf43d49a48e563bab4
Flambii <flambiix@gmail.com>
2017-02-07 10:21:46 +0100

    correction bug css

--- projet/settings.py
+++ projet/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (
                     # Put strings here, like "/home/html/static" or "C:/www/django/static".
                     # Always use forward slashes, even on Windows.
                     # Don't forget to use absolute paths, not relative paths.
-                    '/../socialnetwork/static/',
+                    './socialnetwork/static/',
                     )
 
 # Static files (CSS, JavaScript, Images)

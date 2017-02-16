a669e40e55ddfaea8560fedba07ac4de415585c6
Hactogeek <tony.marteau@outlook.com>
2017-02-16 11:48:35 +0100

    Ajout du modèle pour l'ajout d'amis

--- socialnetwork/admin.py
+++ socialnetwork/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 #test
 # Register your models here.
+
+from .models import Friend
--- socialnetwork/models.py
+++ socialnetwork/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+# Importation des modèles
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Friend(models.Model):
+	ajouteur = models.OneToOneField(User, related_name='ajouteur')
+	ajoute = models.OneToOneField(User, related_name='ajoute')
+	confirme = models.BooleanField(default=False)
+	add_date = models.DateField()

211339a646c4f78219867f6defdd0fce6297e747
Flambii <flambiix@gmail.com>
2017-03-13 23:18:54 +0100

    fixe page rencontre

--- socialnetwork/forms.py
+++ socialnetwork/forms.py
@@ -24,7 +24,7 @@ class informationUserForm(forms.Form):
 	age = forms.IntegerField(label='Age', min_value=18, max_value=100, required=True)
 	localisation = forms.ChoiceField(label='Localisation', widget=forms.Select, choices=DEPT_CHOICES, required=True)
 	profession = forms.CharField(label='Profession', min_length=2, max_length=100, required=False)
-	description = forms.CharField(label='Description', widget=forms.Textarea(attrs={'rows': 3}), max_length=250, required=False)
+	description = forms.CharField(label='Description', widget=forms.Textarea(attrs={'rows': 2}), max_length=250, required=False)
 
 class loginForm(forms.Form):
 	username = forms.CharField(label='Pseudo',min_length=2, max_length=100)
